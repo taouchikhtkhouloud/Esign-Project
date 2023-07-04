bd4a905eb1fc03e7f75fc938e4f3c759c14d61b8
Khouloud TAOUCHIKHT <92301300+taouchikhtkhouloud@users.noreply.github.com>
2023-07-04 00:15:22 +0000

    change the logo

--- Esign/src/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Content/Home.razor.g.cs
+++ Esign/src/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Content/Home.razor.g.cs
@@ -286,7 +286,6 @@ using Esign.Client.Extensions;
 #line hidden
 #nullable disable
     [Microsoft.AspNetCore.Components.RouteAttribute("/home")]
-    [Microsoft.AspNetCore.Components.RouteAttribute("/")]
     public partial class Home : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998

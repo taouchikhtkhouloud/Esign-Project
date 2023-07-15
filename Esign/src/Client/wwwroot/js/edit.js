553ad870760b71eceee702a0b13e3be32d6e347c
Khouloud TAOUCHIKHT <92301300+taouchikhtkhouloud@users.noreply.github.com>
2023-07-15 22:12:16 +0000

    create pdf and download it

--- Esign/src/Client/wwwroot/js/edit.js
+++ Esign/src/Client/wwwroot/js/edit.js
@@ -10,6 +10,32 @@
         size: fontSize,
         color: rgb(0, 0.53, 0.71),
     });
+    const jpgUrl = 'https://pdf-lib.js.org/assets/cat_riding_unicorn.jpg';
+    const pngUrl = 'https://pdf-lib.js.org/assets/minions_banana_alpha.png';
+
+    const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer());
+    const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer());
+
+
+    const jpgImage = await pdfDoc.embedJpg(jpgImageBytes);
+    const pngImage = await pdfDoc.embedPng(pngImageBytes);
+
+    const jpgDims = jpgImage.scale(0.5);
+    const pngDims = pngImage.scale(0.5);
+
+
+    page.drawImage(jpgImage, {
+        x: page.getWidth() / 2 - jpgDims.width / 2,
+        y: page.getHeight() / 2 - jpgDims.height / 2 + 250,
+        width: jpgDims.width,
+        height: jpgDims.height,
+    });
+    page.drawImage(pngImage, {
+        x: page.getWidth() / 2 - pngDims.width / 2 + 75,
+        y: page.getHeight() / 2 - pngDims.height + 250,
+        width: pngDims.width,
+        height: pngDims.height,
+    });
 
     const pdfBytes = await pdfDoc.save();
 
